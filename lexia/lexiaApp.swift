07e28d0a437d10d4f3e6a40037adc3059297a69a
Ibrahim Hashme <hi@ibbi.io>
2023-03-26 17:48:17 -0400

    Initial Commit

---
+++ lexia/lexiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  lexiaApp.swift
+//  lexia
+//
+//  Created by ibbi on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct lexiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
