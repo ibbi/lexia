a659d0d4a1ff8ba9da8d13246519b29cf9bd6a2e
Ibrahim Hashme <hi@ibbi.io>
2023-04-22 14:53:12 -0400

    messig around

--- keyboard/KeyboardActionHandler.swift
+++ keyboard/KeyboardActionHandler.swift
@@ -8,4 +8,42 @@
 import KeyboardKit
 import UIKit
 
-class KeyboardActionHandler: StandardKeyboardActionHandler {}
+class KeyboardActionHandler: StandardKeyboardActionHandler {
+    
+    
+    
+//    override func action(
+//        for gesture: KeyboardGesture,
+//        on action: KeyboardAction
+//    ) -> KeyboardAction.GestureAction? {
+//        switch (gesture, action) {
+//        case (.release, .dictation):
+//            return replacementDictation(for: action)
+//        default:
+//            return super.action(for: gesture, on: action)
+//        }
+//    }
+//    
+//    
+//    
+//    
+//    func replacementDictation(for action: KeyboardAction) -> KeyboardAction? {
+//        return KeyboardAction.character("j")
+//    }
+    
+    //    override func action(
+    //        for gesture: KeyboardGesture,
+    //        on action: KeyboardAction
+    //    ) -> KeyboardAction? {
+    //        let standard = super.action(for: gesture, on: action)
+    //        switch action {
+    //        case .dictation: return replacementDictation(for: gesture, on: action) ?? standard
+    //        default: return standard
+    //        }
+    //
+    //    }
+    //
+    //
+}
+
+
