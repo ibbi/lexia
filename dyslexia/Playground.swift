289fd2c722dd58238bf5d2d215a2eb968d1f79e7
Ibrahim Hashme <hi@ibbi.io>
2023-05-11 17:26:44 -0400

    add dummy views to playground

--- dyslexia/Playground.swift
+++ dyslexia/Playground.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct Playground: View {
+    @FocusState private var isInputFocused: Bool
+    @State private var inputText: String = ""
+    
     var body: some View {
-        NavigationView{
-            Text("Navved and enabled!")
+        VStack {
+            Text("Try yelling")
+                .font(.title)
+            
+            Text("Now try to whisper")
+                .font(.title)
+            
+            TextField("Enter text", text: $inputText)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .focused($isInputFocused)
+            
+            Spacer()
         }
+        .onAppear {
+           isInputFocused = true
+       }
     }
 }
-
 struct Playground_Previews: PreviewProvider {
     static var previews: some View {
         Playground()
