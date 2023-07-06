75185f35c823bec8a1580ff329da58be8290c42e
Ibrahim Hashme <hi@ibbi.io>
2023-07-05 22:02:11 -0400

    Add custom quick rompt support to BE

--- basic-bundle-long-queen-51be/src/index.ts
+++ basic-bundle-long-queen-51be/src/index.ts
@@ -66,7 +66,8 @@ async function handleTransformerRequest(request: Request): Promise<Response> {
   try {
     const requestBody = await request.json();
     const userMessage = requestBody.message;
-    const promptWrapped = `Please rewrite this: \n"${userMessage}"\n:`;
+    const userPrompt = requestBody.prompt || 'Please rewrite this: ';
+    const promptWrapped = `${userPrompt}\n"${userMessage}"\n:`;
 
     const response = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
@@ -132,14 +133,12 @@ async function handleVoiceEdit(request: Request): Promise<Response> {
     const { text } = await whisperResponse.json();
 
     const promptWrapped = `Apply the following instructions:\n---\n${text}\n---\n\nto the following text:\n---\n${contextText}\n---\n`;
-    console.log(promptWrapped);
     const gptResponse = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages: [{ role: 'user', content: promptWrapped }],
     });
 
     const assistantMessage = gptResponse.data.choices[0].message?.content.replace(/^"(.*)"$/, '$1');
-    console.log(assistantMessage);
 
     return new Response(assistantMessage, {
       headers: { 'Content-Type': 'text/plain' },
@@ -174,7 +173,6 @@ async function handleWhisperWebSocket(request: Request): Promise<Response> {
 
     const data = await response.json();
     const text = data['text'] || '';
-    console.log(response, data, text);
     // @ts-ignore
     server.send(text);
   });
